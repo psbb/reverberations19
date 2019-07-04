8260976dc012fee0e75a0bfd62d8597654b71356
psbb <52292841+psbb@users.noreply.github.com>
2019-07-04 20:58:19 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -181,56 +181,9 @@ $(document).ready(function()
 		}
 	}
 
-	/*
+	
 
-	6. Init Timer
-
-	*/
-
-	function initTimer()
-	{
-		if($('.timer_list').length)
-    	{
-    		// Uncomment line below and replace date
-	    	 var target_date = new Date("July 05, 2019").getTime();
-
-	    	// comment lines below
-	    	// var date = new Date();
-	    	// date.setDate(date.getDate() + 6);
-	    	// var target_date = date.getTime();
-	    	//----------------------------------------
-
-			// variables for time units
-			var days, hours, minutes, seconds;
-
-			var d = $('#day');
-			var h = $('#hour');
-			var m = $('#minute');
-			var s = $('#second');
-
-			setInterval(function ()
-			{
-			    // find the amount of "seconds" between now and target
-			    var current_date = new Date().getTime();
-			    var seconds_left = (target_date - current_date) / 1000;
-
-			    // do some time calculations
-			    days = parseInt(seconds_left / 86400);
-			    seconds_left = seconds_left % 86400;
-
-			    hours = parseInt(seconds_left / 3600);
-			    seconds_left = seconds_left % 3600;
-
-			    minutes = parseInt(seconds_left / 60);
-			    seconds = parseInt(seconds_left % 60);
-
-			    // display result
-			    d.text(days);
-			    h.text(hours);
-			    m.text(minutes);
-			    s.text(seconds);
-
-			}, 1000);
+	
     	}
 	}
 
